feb8ef20d520ea312df43eb70bff9d4212c31a56
Krystian Sikora <92646188+krystian-sikora@users.noreply.github.com>
2023-11-23 22:50:25 +0100

    oopsie, remove bearer token

--- src/app/movie-data.service.ts
+++ src/app/movie-data.service.ts
@@ -10,7 +10,7 @@ export class MovieDataService {
   constructor(private http: HttpClient) { }
 
   readonly API_URL = 'https://api.themoviedb.org/3'
-  readonly API_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTdlYzRjNjEzN2IyMGYzM2NiNWM3MTEwNWE2ODY5OCIsInN1YiI6IjY1NWU1OGQ5ODNlZTY3MDFmNjI1NjYwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BOH44WFH-w8HVURt-6gP6RNzqEOedrESiE4AFiY5iFE'
+  readonly API_TOKEN = 'token :)'
 
   getHeaders() {
     return new HttpHeaders()
