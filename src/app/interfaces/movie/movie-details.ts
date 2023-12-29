f52ad5e34c7bc788a648498d29a3845cb82cdd7d
krystian-sikora <s223097@student.uek.krakow.pl>
2023-12-29 23:07:20 +0100

    Details refactor

---
+++ src/app/interfaces/movie/movie-details.ts
@@ -0,0 +1,40 @@
+import { Genre } from "../genre"
+import { ProductionCompany } from "../production-company"
+import { ProductionCountry } from "../production-country"
+import { SpokenLanguage } from "../spoken-language"
+
+export interface MovieDetails {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: any
+  budget: number
+  genres: Genre[]
+  homepage: string
+  id: number
+  imdb_id: string
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  release_date: string
+  revenue: number
+  runtime: number
+  spoken_languages: SpokenLanguage[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+
+
+
+
+
+
+
