f01e5ee868f25b4a2cc4838bc5e05002cb0bf8de
Krystian Sikora <92646188+krystian-sikora@users.noreply.github.com>
2023-12-29 23:09:01 +0100

    details refactor (#8)

--- src/app/interfaces/people/person-details.ts
+++ src/app/interfaces/people/person-details.ts
@@ -1,10 +1,18 @@
-import { MovieDetails } from "../movie/movie-details";
 
 export interface PersonDetails {
-    id: number;
-    name: string;
-    known_for: Array<MovieDetails>
-    profile_path: string;
-    popularity: number;
-    biography: string;
-}
+    adult: boolean
+    also_known_as: string[]
+    biography: string
+    birthday: string
+    deathday: any
+    gender: number
+    homepage: any
+    id: number
+    imdb_id: string
+    known_for_department: string
+    name: string
+    place_of_birth: string
+    popularity: number
+    profile_path: string
+  }
+  
