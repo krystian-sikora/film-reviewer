608eb0953d93f55c7440784d2313af0831278ee1
Krzysztof222859 <s222859@student.uek.krakow.pl>
2024-01-08 14:43:33 +0100

    removes secrets

--- src/environments/environments.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    API_URL: 'https://api.themoviedb.org',
-    API_KEY: '7cbbea6257df6d0879a0dd6c644f3705',
-    API_TOKEN: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2JiZWE2MjU3ZGY2ZDA4NzlhMGRkNmM2NDRmMzcwNSIsInN1YiI6IjY1Njc0ZmE1MTU2Y2M3MDBlYmMxYWQ3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.smDVCUQiE4FFcqB16jU709ogHpXwBC-DAemstNUyhS8'
-};
